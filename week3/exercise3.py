1a2778cb0c69fe45d2d86e669184c3ebc490289c
Harley <harley.trappitt@gmail.com>
2017-04-06 06:39:26 +1000

    Make guessing game for Exercise3.

--- week3/exercise3.py
+++ week3/exercise3.py
@@ -28,7 +28,31 @@ def advancedGuessingGame():
     Remember to think modular. Try to keep your functions small and single
     purpose if you can!
     """
-    pass
+
+    print("\nwelcome to the guessing game!")
+    print("A number between 0 and _ ?")
+    upperBound = raw_input("Enter an upper bound: ")
+    print("OK then, a number between 0 and {} ?".format(upperBound))
+    upperBound = int(upperBound)
+
+    actualNumber = random.randint(0, upperBound)
+
+    guessed = False
+
+    while not guessed:
+        try:
+            guessedNumber = int(raw_input("guess a number: "))
+            print("you guessed {},".format(guessedNumber),)
+            if guessedNumber == actualNumber:
+                print("you got it!! It was {}".format(actualNumber))
+                guessed = True
+            elif guessedNumber < actualNumber:
+                print("too small, try again ")
+            else:
+                print("too big, try again   ")
+        except Exception as e:
+            print ("Sorry, that's not an actual number, try again!" + "\n", e)
+    return "You got it!"
 
 
 if __name__ == "__main__":

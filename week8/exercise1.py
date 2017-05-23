30e6aba8ed64c2a7d24f3a288455f666432af04c
Harley <harley.trappitt@gmail.com>
2017-05-23 19:16:57 +1000

    Test Completed (To the best of my ability).

--- week8/exercise1.py
+++ week8/exercise1.py
@@ -9,7 +9,7 @@ You've got an hour.
 """
 from __future__ import division
 from __future__ import print_function
-import time
+# import time
 
 
 def greet(name="Towering Timmy"):
@@ -18,7 +18,7 @@ def greet(name="Towering Timmy"):
     return a string of "Hello" and the name argument.
     E.g. if given as "Towering Timmy" it should return "Hello Towering Timmy"
     """
-    pass
+    return ("Hello " + name)
 
 
 def three_counter(input_list=[1, 4, 3, 5, 7, 1, 3, 2, 3, 3, 5, 3, 7]):
@@ -27,7 +27,12 @@ def three_counter(input_list=[1, 4, 3, 5, 7, 1, 3, 2, 3, 3, 5, 3, 7]):
     Return an integer.
     TIP: the test will use a different input_list, so don't just return 5
     """
-    pass
+    count = 0
+    list_length = len(input_list)
+    for x in range(0, list_length):
+        if input_list[x] == 3:
+            count += 1
+    return count
 
 
 def fizz_buzz():
@@ -45,7 +50,15 @@ def fizz_buzz():
     if it is. E.g. [1, 2, "Fizz", 4, "Buzz", 6, 7, ...]
     """
     fizzBuzzList = []
-    # your code here
+    for x in range(1, 101):
+        if (x % 3 == 0) and (x % 5 == 0):
+            fizzBuzzList.append("FizzBuzz")
+        elif (x % 3 == 0):
+            fizzBuzzList.append("Fizz")
+        elif (x % 5 == 0):
+            fizzBuzzList.append("Buzz")
+        else:
+            fizzBuzzList.append(x)
     return fizzBuzzList
 
 
@@ -56,7 +69,14 @@ def put_behind_bars(input_string="very naughty boy"):
     e.g. "very naughty boy" should return "|v|e|r|y| |n|a|u|g|h|t|y| |b|o|y|"
     TIP: make sure that you have a pipe on both ends of the string.
     """
-    pass
+    input_string_list = list(input_string)
+    input_string_length = len(input_string_list)
+    string_list = ["|"]
+    for x in range(0, input_string_length):
+        string_list.append(input_string_list[x])
+        string_list.append("|")
+    pipe_string = "".join(string_list)
+    return (pipe_string)
 
 
 def pet_filter(letter="a"):
@@ -69,7 +89,11 @@ def pet_filter(letter="a"):
             "bali cattle", "gayal", "turkey", "goldfish", "rabbit", "koi",
             "canary", "society finch", "fancy mouse", "siamese fighting fish",
             "fancy rat and lab rat", "mink", "red fox", "hedgehog", "guppy"]
-    pass
+    pets_list = []
+    for x in range(0, len(pets)):
+        if letter in pets[x]:
+            pets_list.append(pets[x])
+    return (pets_list)
 
 
 def best_letter_for_pets():
@@ -80,7 +104,14 @@ def best_letter_for_pets():
     """
     import string
     the_alphabet = string.lowercase
-    pass
+    alphabet_list = list(the_alphabet)
+    count = 0
+    for x in range(0, 26):
+        best_letter = pet_filter(alphabet_list[x])
+        if len(best_letter) > count:
+            count = len(best_letter)
+            best_letter_list = alphabet_list[x]
+    return (best_letter_list)
 
 
 def make_filler_text_dictionary():
@@ -98,7 +129,19 @@ def make_filler_text_dictionary():
     TIP: you'll need the requests library
     """
     import requests
-    pass
+    baseURL = "http://www.setgetgo.com/randomword/get.php?len="
+    filler_text_dic = {}
+    arg_name = []
+    word_count = 3
+    for x in range(3, 8):
+        if word_count > 0:
+            url = baseURL + str(x)
+            r = requests.get(url)
+            arg_name.append(r.text)
+            word_count = word_count - 1
+        filler_text_dic.update({x: arg_name})
+    print (filler_text_dic)
+    return (filler_text_dic)
 
 
 def random_filler_text(number_of_words=200):
@@ -113,7 +156,7 @@ def random_filler_text(number_of_words=200):
     Bonus: extra mark if you get the paragraph to start with a
            capital letter and end with a full stop.
     """
-    import random
+#    import random
     pass
 
 
